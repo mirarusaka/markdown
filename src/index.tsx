be9a74f26f6d7378dae8cb2e29cf1413f3da4cd9
69safe <endokun32@yahoo.co.jp>
2021-11-29 20:45:51 +0900

    editor.tsxを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import { createGlobalStyle } from 'styled-components'
+import { Editor } from './pages/editor'
 
-const Main = (<h1>Markdown Editor</h1>)
+const GlobalStyle = createGlobalStyle`
+    body * {
+      box-sizing: border-box;
+    }
+  `
+  
+  const Main = (
+    <>
+      <GlobalStyle />
+      <Editor />
+    </>
+  )
 
+//Main変数の値をId appに適用する render = 追加
 render(Main, document.getElementById('app'))
