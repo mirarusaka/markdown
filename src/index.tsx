f8ae3455a44e362f4b592116782a3d0e23c6d99d
69safe <endokun32@yahoo.co.jp>
2021-12-04 10:40:10 +0900

    4-3完了

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { createGlobalStyle } from 'styled-components'
 import { Editor } from './pages/editor'
 import { History } from './pages/history'
+import { useStateWithStorage } from './hooks/use_state_with_storage'
 import {
   HashRouter as Router,
   Switch,
@@ -15,21 +16,33 @@ const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
     }
   `
-  
-  const Main = (
+const StorageKey = '/editor:text'
+
+const Main: React.FC = () => {
+  const [text, setText] = useStateWithStorage('', StorageKey)
+
+  return (
     <>
       <GlobalStyle />
       <Router>
-        <Route exact path="/editor">
-          <Editor />
-        </Route>
-        <Route exact path="/history">
-          <History />
-        </Route>
-        <Redirect to="/editor" path="*" />
+        <Switch>
+          <Route exact path="/editor">
+            <Editor
+              text={text}
+              setText={setText}
+            />
+          </Route>
+          <Route exact path="/history">
+            <History
+              setText={setText}
+            />
+          </Route>
+          <Redirect to="/editor" path="*" />
+        </Switch>
       </Router>
     </>
   )
+}
 
-//Main変数の値をId appに適用する render = 追加
-render(Main, document.getElementById('app'))
+render(<Main />, document.getElementById('app'))
+
